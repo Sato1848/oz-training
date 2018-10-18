304635e0df1cc20bd3a0fafb9e36228e0f0af6fa
ysato <y.sato@goalist.co.jp>
2018-10-18 12:48:00 +0900

    研修課題３－２

--- src/main/java/jp/co/goalist/mediamapper/MediaMapper.java
+++ src/main/java/jp/co/goalist/mediamapper/MediaMapper.java
@@ -1,33 +1,70 @@
 package jp.co.goalist.mediamapper;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MediaMapper {
 
 	static Map<String,String> mediaMap = new HashMap<>();
+	static List<String> outputList = new ArrayList<>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		String path = "/training/media_mst.csv"; //読み込むファイルのパス
-		mapDatas(path);
+		String master = "/training/media_mst.csv"; //マスタのパス
+		mapDatas(master);
+
+		String target = "/training/map_training.csv"; //マスタをあてるファイルのパス
+		String output = "/training/map_training01.csv"; //出力先のパス
+		correctMediaNames(target, output);
+
 
 	}
 
-	private static void mapDatas(String path){
+	private static void mapDatas(String master){
 
-		Path filePath = Paths.get(path);
-		try (BufferedReader br = Files.newBufferedReader(filePath)) {
-		    String line;
+		Path masterPath = Paths.get(master);
+
+		try (BufferedReader br = Files.newBufferedReader(masterPath)) {
+		    String line = br.readLine(); //1行目はとばす
 		    while ((line = br.readLine()) != null) {
 		        String[] elems = line.split(","); //カンマで分割
 		        mediaMap.put(elems[0], elems[1]); //媒体コードをkey、媒体名をvalueとしてマップに格納
 
+		    }
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+
+	private static void correctMediaNames(String target, String output) throws IOException {
+
+		Path targetPath = Paths.get(target);
+		Path outputPath = Paths.get(output);
+
+		Files.deleteIfExists(outputPath);
+		Files.createFile(outputPath);
+
+
+		try (BufferedReader br = Files.newBufferedReader(targetPath);
+		    BufferedWriter bw = Files.newBufferedWriter(outputPath)) {
+			String correctedLine = null;
+		    String line;
+		    while ((line = br.readLine()) != null) {
+
+		    	String[] lineSplit = line.split(","); //カンマで分割
+		    	lineSplit[2] = mediaMap.get(lineSplit[2]); //媒体名を置換
+		    	correctedLine = String.join(",", lineSplit); //カンマで再連結
+
+		    	bw.write(correctedLine);
+			    bw.newLine();
 		    }
 		} catch (IOException e) {
 		    e.printStackTrace();
