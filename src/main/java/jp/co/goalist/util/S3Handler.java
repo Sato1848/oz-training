c6436cbdbd893c15bc0c2cdf0b0ff14260dc6f6d
ysato <y.sato@goalist.co.jp>
2018-10-21 19:13:01 +0900

    研修課題４

--- src/main/java/jp/co/goalist/util/S3Handler.java
+++ src/main/java/jp/co/goalist/util/S3Handler.java
@@ -13,19 +13,19 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 public class S3Handler {
 	public static void main(String[] args){
-        
+
 		String region = "ap-northeast-1"; //リージョン
-        String bucketName = "goalist-dev-sandbox"; //バケット名
-        String keyName = "oz-training/media_mst.csv"; //オブジェクトキー
+        String bucketName = "bucket_name"; //バケット名
+        String keyName = "object_name"; //オブジェクトキー
         String dest = "/training/media_mst.csv"; //ダウンロード先のファイルパス
-        
-        downloadObject(region, keyName, bucketName, dest); 
-       
+
+        downloadObject(region, keyName, bucketName, dest);
+
         System.out.println("Done!");
     }
-	
+
 	private static void downloadObject(String region, String keyName, String bucketName, String dest) {
-		
+
 		 System.out.format("Downloading %s from S3 bucket %s...\n", keyName, bucketName);
 	        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
 	                .withRegion(region) //リージョンをセット
@@ -33,12 +33,12 @@ public class S3Handler {
 
 	        try {
 	            S3Object o = s3Client.getObject(bucketName, keyName);
-	            S3ObjectInputStream s3is = o.getObjectContent(); 
+	            S3ObjectInputStream s3is = o.getObjectContent();
 	            FileOutputStream fos = new FileOutputStream(new File(dest));
 	            byte[] read_buf = new byte[1024];
 	            int read_len = 0;
 	            while ((read_len = s3is.read(read_buf)) > 0) {
-	                fos.write(read_buf, 0, read_len); 
+	                fos.write(read_buf, 0, read_len);
 	            }
 	            s3is.close();
 	            fos.close();
